be0c0410cabbf422e5b478dc5bff756650acf9aa
rubens <rubensfqc@gmail.com>
2025-02-13 17:08:10 +0100

    customizing admin view

--- quotation_prj/quotation_app/admin.py
+++ quotation_prj/quotation_app/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Client
 
-# Register your models here.
+@admin.register(Client)
+class ClientAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'whatsapp')  # Fields to display in the list view
+    search_fields = ('name', 'email')  # Fields to enable searching
+    list_filter = ('name',)  # Fields to enable filtering
