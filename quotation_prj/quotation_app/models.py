84e04e00a2c62e76e1fcb0c4ab68664a57f5f4bc
Rubens at HP Elitedesk <rubensfqc@gmail.com>
2025-02-13 20:52:19 +0000

    adding quotation model

--- quotation_prj/quotation_app/models.py
+++ quotation_prj/quotation_app/models.py
@@ -25,5 +25,16 @@ class Client(models.Model):
 
     def __str__(self):
         return self.name
-    
+
+class Quotation(models.Model):
+    client = models.ForeignKey(Client, on_delete=models.CASCADE)
+    product_name = models.CharField(max_length=100)
+    quantity = models.IntegerField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def total_price(self):
+        return self.quantity * self.price
+
+    def __str__(self):
+        return f"{self.product_name} - {self.quantity}"
 
