ff2162d420fa42abb6f3545dcd231c042f9c8b2d
rubens <rubensfqc@gmail.com>
2025-02-13 17:09:27 +0100

    creating a model for Client with 3 fields

--- quotation_prj/quotation_app/models.py
+++ quotation_prj/quotation_app/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.core.validators import RegexValidator
 
-# Create your models here.
+# Validator for numbers in the format "+11 91234-5678" or "11 91234-5678"
+brazilian_phone_validator = RegexValidator(
+    regex=r'^\+?(\d{2})\s9\d{4}-\d{4}$',
+    message="Enter a valid phone number (e.g., '+11 91234-5678' or '11 91234-5678')"
+)
+
+class Client(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+
+    # Telephone field (Ensures only digits and + sign)
+    whatsapp = models.CharField(max_length=11)
+    #     max_length=11,  # 2-digit area code + 9-digit number 
+    #     validators=[brazilian_phone_validator], 
+    #     help_text="Format: +11 91234-5678 or 11 91234-5678"
+    # )
+
+    # def save(self, *args, **kwargs):
+    #     # Remove all non-numeric characters before saving to the database
+    #     self.whatsapp = ''.join(filter(str.isdigit, self.whatsapp))
+    #     super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.name
+    
+
