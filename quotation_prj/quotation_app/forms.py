ef57160e4b23e80fcb63439937d66eb5e63d447f
rubens <rubensfqc@gmail.com>
2025-02-13 17:08:52 +0100

    adding form for client

---
+++ quotation_prj/quotation_app/forms.py
@@ -0,0 +1,14 @@
+from django import forms
+from .models import Client
+
+class ClientForm(forms.ModelForm):
+    class Meta:
+        model = Client
+        fields = ['name', 'email', 'whatsapp']
+
+    def clean_whatsapp(self):
+        phone = self.cleaned_data['whatsapp']
+        phone = ''.join(filter(str.isdigit, phone))  # Remove non-numeric characters
+        if len(phone) != 11:
+            raise forms.ValidationError("Enter a valid Brazilian phone number (e.g., 11 91234-5678)")
+        return phone  # Store only digits
