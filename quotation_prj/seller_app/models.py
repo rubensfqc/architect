c2a3adcc83ae4e305bb4bd82a5407a152d5f4e76
Rubens Ninkear <contact.plat4u@gmail.com>
2025-05-14 06:33:03 +0100

    Adding Seller and making it visible in the Admin page

---
+++ quotation_prj/seller_app/models.py
@@ -0,0 +1,34 @@
+from django.db import models
+from django.contrib.auth.models import User, AbstractUser, Group, Permission
+
+class Seller(AbstractUser): #models.Model):
+    #user = models.OneToOneField(User, on_delete=models.CASCADE)  # Links Seller to a Django user
+    email = models.EmailField(unique=True, default="default@example.com")
+    name = models.CharField(max_length=100, default="defaultname")
+    phone_number = models.CharField(max_length=15, blank=True, null=True)
+    profile_picture = models.ImageField(upload_to='profile_pics/', blank=True, null=True)
+
+    groups = models.ManyToManyField(
+        Group,
+        related_name='seller_set',  # make this unique
+        blank=True,
+        help_text='The groups this user belongs to.',
+        verbose_name='groups',
+    )
+    user_permissions = models.ManyToManyField(
+        Permission,
+        related_name='seller_user_permissions',  # make this unique
+        blank=True,
+        help_text='Specific permissions for this user.',
+        verbose_name='user permissions',
+    )
+
+    #password = "defaultpassword123"  # Set a default password
+    #self.set_password(password)  # Hash the password
+    #self.save()  # Save the user
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+
+    def __str__(self):
+        return self.email
