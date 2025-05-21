d364c8d1d646e372b383abc4763620f1fb0f2c58
Rubens Carvalho <rubens.carvalho@asml.com>
2025-05-21 14:21:30 +0200

    changing seller to pro in the URL

--- quotation_prj/quotation_prj/urls.py
+++ quotation_prj/quotation_prj/urls.py
@@ -27,7 +27,7 @@ urlpatterns = [
     path('quotation/<int:client_id>/', views.quotation_page, name='quotation_page'),
     path('generate-pdf/<int:quotation_id>/', views.generate_pdf, name='generate_pdf'),
     path('email/', include('email_app.urls')),
-    path('seller/', include('seller_app.urls')),
+    path('pro/', include('seller_app.urls')),
     path('accounts/', include('django.contrib.auth.urls')), # o logout da base.html estah sendo achado por aqui
     #path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
     #path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
--- quotation_prj/seller_app/urls.py
+++ quotation_prj/seller_app/urls.py
@@ -3,7 +3,7 @@ from . import views
 from .views import SignUpView
 
 urlpatterns = [
-    #path('', views.seller_dashboard, name='seller_dashboard'),
+    path('', views.seller_dashboard, name='seller_dashboard'),
     path('dashboard/', views.seller_dashboard, name='seller_dashboard'),
     path('add-product/', views.add_product, name='add_product'),
     path('edit/<int:product_id>/', views.edit_product, name='edit_product'),
