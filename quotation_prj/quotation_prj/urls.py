67d5cfd0040c2b84cfce778c35ce6b1dd58a317b
Rubens Ninkear <contact.plat4u@gmail.com>
2025-06-18 00:28:20 +0200

    changing index page of the app

--- quotation_prj/quotation_prj/urls.py
+++ quotation_prj/quotation_prj/urls.py
@@ -25,8 +25,8 @@ from seller_app import views as seller_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home_view, name='home'),  # Set this as the home view
-    path('search/', seller_views.slug_search, name='slug_search'),  # Search by slug
+    path('pages', views.home_view, name='home'),  # Set this as the home view
+    path('', seller_views.slug_search, name='slug_search'),  # Search by slug
     path('autocomplete/', seller_views.slug_autocomplete, name='slug_autocomplete'), 
     path('lp', views.landing_page, name='landing_page'),
     path('quotation/<int:client_id>/', views.quotation_page, name='quotation_page'),
